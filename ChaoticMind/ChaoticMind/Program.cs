6653548a74577f43cf0aa477bec61da505e9ac85
Chris Cooper <chris@quixotic-chris.com>
2012-02-04 03:52:02 -0500

    Renamed Main game class

--- ChaoticMind/ChaoticMind/Program.cs
+++ ChaoticMind/ChaoticMind/Program.cs
@@ -10,7 +10,7 @@ namespace ChaoticMind
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (ChaoticMindGame game = new ChaoticMindGame())
             {
                 game.Run();
             }
