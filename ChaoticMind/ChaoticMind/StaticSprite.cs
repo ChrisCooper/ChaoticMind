14143f03e6e4cb966920165debd106aa88c34cdf
Chris Cooper <chris@quixotic-chris.com>
2012-03-05 17:41:32 -0500

    Actually added static sprite sheet. Todo: learn how to amend last commit...

---
+++ ChaoticMind/ChaoticMind/StaticSprite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChaoticMind
+{
+    class StaticSprite : AnimatedSprite
+    {
+        public StaticSprite(String resourcePrefix)
+            : base(resourcePrefix, 64, 64, 1)
+        {
+        }
+    }
+}
