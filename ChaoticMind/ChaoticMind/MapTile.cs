db8053d5f79c540ec70b281f4bb7bb6b6e524bb3
Chris Cooper <chris@quixotic-chris.com>
2012-03-06 17:56:28 -0500

    Added rest of Door files...

--- ChaoticMind/ChaoticMind/MapTile.cs
+++ ChaoticMind/ChaoticMind/MapTile.cs
@@ -21,18 +21,28 @@ namespace ChaoticMind
         WEST = 1 << 3
     };
 
+    //This is only used to index the door array internally. All other accesses are through theDoorDirection enum
+    enum DoorIndex
+    {
+        NORTH = 0,
+        SOUTH = 1,
+        EAST = 2,
+        WEST = 3
+    };
+
     class MapTile : DrawableGameObject
     {
 
         public const float TileSideLength = 15.0f;
 
-        static List<List<Vertices>> WallVertices;
+        static List<List<Vertices>> _WallVertices;
+        static List<List<Vertices>> _DoorVertices;
 
-        int _xGridCoord;
-        int _yGridCoord;
+        DoorDirection _openDoors;
 
         public MapTile(World world, Vector2 startingPosition, DoorDirection openDoors) : base(world, startingPosition)
         {
+            _openDoors = openDoors;
 
             _sprite = new StaticSprite(resourseStringFromDoorConfiguration(openDoors));
 
@@ -44,23 +54,42 @@ namespace ChaoticMind
 
             _body.BodyType = BodyType.Kinematic;
 
-            //_body.AngularVelocity = 0.1f;
-           
-            // This method creates a body (has mass, position, rotation),
-            // as well as a rectangular fixture, which is just a shape stapled to the body.
-            // The fixture is what collides with other objects and impacts how the body moves
-            //_body = BodyFactory.CreateRectangle(world, TileSideLength, TileSideLength, 1.0f);
-            //_body.BodyType = BodyType.Kinematic;
             _body.Position = startingPosition;
+        }
+
+        
+        private static List<Vertices> DoorVertices(DoorDirection door) {
+            return _DoorVertices[(int)DoorDiretionToIndex(door)];
+        }
+
+        private static DoorIndex DoorDiretionToIndex(DoorDirection direction)
+        {
+            switch (direction) {
+                case DoorDirection.NORTH:
+                    return DoorIndex.NORTH;
+                case DoorDirection.SOUTH:
+                    return DoorIndex.SOUTH;
+                case DoorDirection.EAST:
+                    return DoorIndex.EAST;
+                case DoorDirection.WEST:
+                    return DoorIndex.WEST;
+                default:
+                    return (DoorIndex)(-1);
+            }
         }
 
         private void loadWallFixtures()
         {
-            foreach (List<Vertices> list in WallVertices)
+            foreach (List<Vertices> list in _WallVertices)
             {
                 List<Fixture> compund = FixtureFactory.AttachCompoundPolygon(list, 1.0f, _body);
             }
         }
+
+        private void loadDoorFixture(DoorDirection door)
+        {
+            List<Fixture> compund = FixtureFactory.AttachCompoundPolygon(DoorVertices(door), 1.0f, _body);
+         }
 
         private String resourseStringFromDoorConfiguration(DoorDirection config)
         {
@@ -79,14 +108,54 @@ namespace ChaoticMind
 
         static MapTile()
         {
-            WallVertices = new List<List<Vertices>>();
+            loadWallVertices();
+
+            _DoorVertices = new List<List<Vertices>>();
+
+            List<StaticSprite> doorSprites = new List<StaticSprite>();
+            doorSprites.Add(new StaticSprite("TileGeometry/DoorGeometryN"));
+            doorSprites.Add(new StaticSprite("TileGeometry/DoorGeometryS"));
+            doorSprites.Add(new StaticSprite("TileGeometry/DoorGeometryE"));
+            doorSprites.Add(new StaticSprite("TileGeometry/DoorGeometryW"));
+
+
+            foreach (StaticSprite sprite in doorSprites)
+            {
+                float pixelsPerMeter = sprite.CurrentTextureBounds.Width / TileSideLength;
+
+                uint[] data = new uint[sprite.Texture.Width * sprite.Texture.Height];
+
+                sprite.Texture.GetData(data);
+
+                //Generate a Polygon
+                Vertices verts = PolygonTools.CreatePolygon(data, sprite.Texture.Width);
+
+                //Center
+                Vector2 centeringVector = new Vector2(-sprite.Texture.Width / 2, -sprite.Texture.Height / 2);
+                verts.Translate(ref centeringVector);
+
+                //Scale
+                Vector2 scale = new Vector2(1 / pixelsPerMeter, 1 / pixelsPerMeter);
+                verts.Scale(ref scale);
+
+                //Since it is a concave polygon, we need to partition it into several smaller convex polygons
+
+                List<Vertices> list = BayazitDecomposer.ConvexPartition(verts);
+
+                _DoorVertices.Add(list);
+            }
+
+        }
+
+        private static void loadWallVertices()
+        {
+            _WallVertices = new List<List<Vertices>>();
 
             List<StaticSprite> wallSprites = new List<StaticSprite>();
             wallSprites.Add(new StaticSprite("TileGeometry/BoxWallGeometryNW"));
             wallSprites.Add(new StaticSprite("TileGeometry/BoxWallGeometryNE"));
             wallSprites.Add(new StaticSprite("TileGeometry/BoxWallGeometrySE"));
             wallSprites.Add(new StaticSprite("TileGeometry/BoxWallGeometrySW"));
-
 
             foreach (StaticSprite sprite in wallSprites)
             {
@@ -111,7 +180,7 @@ namespace ChaoticMind
 
                 List<Vertices> list = BayazitDecomposer.ConvexPartition(verts);
 
-                WallVertices.Add(list);
+                _WallVertices.Add(list);
 
             }
         }
