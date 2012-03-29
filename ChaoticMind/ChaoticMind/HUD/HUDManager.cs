f54509326e9f32c4c198ebc2d95c5c81dcaa89cc
Chris Cooper <chris@quixotic-chris.com>
2012-03-29 03:35:57 -0400

    Extracted minimap frame code to its own class

--- ChaoticMind/ChaoticMind/HUD/HUDManager.cs
+++ ChaoticMind/ChaoticMind/HUD/HUDManager.cs
@@ -13,48 +13,31 @@ namespace ChaoticMind.HUD {
 
         WeaponDisplay _weaponDisplay = new WeaponDisplay();
         HealthDisplay _healthDisplay = new HealthDisplay();
-
-        //MINIMAP
-        StaticSprite _minimapFrame;
-        float _mapFrameScale;
-        Rectangle _mapFrameRect;
-        //The percentage of the full frame image that each side of the frame takes up
-        float _mapFrameWidthFraction = 0.1f;
-        Rectangle _mapRect;
+        MinimapDisplay _minimapDisplay = new MinimapDisplay();
 
         internal void Initialize() {
             _mainInstance = this;
-            setupMinimapInfo();
             _healthDisplay.Initialize();
             _weaponDisplay.Initialize();
+            _minimapDisplay.Initialize();
         }
 
         private void setupMinimapInfo() {
-            _minimapFrame = new StaticSprite("HUD/Minimap_Frame", 1.0f, DrawLayers.HUD_Backgrounds);
-
-            float mapFrameSideLength = Math.Min(Screen.Width / 4.0f, Screen.Height / 3.0f);
-            _mapFrameScale = mapFrameSideLength / _minimapFrame.Texture.Bounds.Width;
-            _mapFrameRect = new Rectangle(0, (int)(Screen.Height - mapFrameSideLength), (int)mapFrameSideLength, (int)mapFrameSideLength);
-
-            float frameWidth = _mapFrameWidthFraction * mapFrameSideLength;
-            _mapRect = new Rectangle((int)(_mapFrameRect.Left + frameWidth), (int)(_mapFrameRect.Top + frameWidth), (int)(_mapFrameRect.Width - 2 * frameWidth), (int)(_mapFrameRect.Height - 2 * frameWidth));
-        }
+            }
 
         private void setupHealthInfo() {
            }
 
         internal void Draw_HUD(SpriteBatch spriteBatch) {
-            //Minimap frame
-            spriteBatch.Draw(_minimapFrame.Texture, _mapFrameRect, _minimapFrame.CurrentTextureBounds, Color.White, 0.0f, Vector2.Zero, SpriteEffects.None, _minimapFrame.DrawLayer);
-
 
             _healthDisplay.DrawDisplay(spriteBatch);
             _weaponDisplay.DrawDisplay(spriteBatch);
+            _minimapDisplay.DrawDisplay(spriteBatch);
 
             }
 
         internal static Rectangle MinimapRect {
-            get { return _mainInstance._mapRect; }
+            get { return _mainInstance._minimapDisplay.MinimapRect; }
         }
     }
 }
---
+++ ChaoticMind/ChaoticMind/HUD/MinimapDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace ChaoticMind.HUD {
+    class MinimapDisplay {
+
+        StaticSprite _minimapFrame;
+        float _mapFrameScale;
+        Rectangle _mapFrameRect;
+        //The percentage of the full frame image that each side of the frame takes up
+        float _mapFrameWidthFraction = 0.1f;
+        Rectangle _mapRect;
+
+        internal void Initialize() {
+            _minimapFrame = new StaticSprite("HUD/Minimap_Frame", 1.0f, DrawLayers.HUD_Backgrounds);
+
+            float mapFrameSideLength = Math.Min(Screen.Width / 4.0f, Screen.Height / 3.0f);
+            _mapFrameScale = mapFrameSideLength / _minimapFrame.Texture.Bounds.Width;
+            _mapFrameRect = new Rectangle(0, (int)(Screen.Height - mapFrameSideLength), (int)mapFrameSideLength, (int)mapFrameSideLength);
+
+            float frameWidth = _mapFrameWidthFraction * mapFrameSideLength;
+            _mapRect = new Rectangle((int)(_mapFrameRect.Left + frameWidth), (int)(_mapFrameRect.Top + frameWidth), (int)(_mapFrameRect.Width - 2 * frameWidth), (int)(_mapFrameRect.Height - 2 * frameWidth));
+        }
+
+        internal void DrawDisplay(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch) {
+            spriteBatch.Draw(_minimapFrame.Texture, _mapFrameRect, _minimapFrame.CurrentTextureBounds, Color.White, 0.0f, Vector2.Zero, SpriteEffects.None, _minimapFrame.DrawLayer);
+
+        }
+
+        internal Rectangle MinimapRect {
+            get { return _mapRect; }
+        }
+    }
+}
